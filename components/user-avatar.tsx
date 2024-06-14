cd01111ddc1c9e9436e458aa524849adfcfdd1f6
Yashika Nagpal <nagpalyashika7@gmail.com>
2024-06-14 10:26:51 +0530

    First commit

---
+++ components/user-avatar.tsx
@@ -0,0 +1,17 @@
+import { Avatar,AvatarImage,AvatarFallback } from "@/components/ui/avatar"
+import { useUser } from "@clerk/nextjs"
+
+export const UserAvatar=()=>{
+    const { user }=useUser();
+        return(
+            <Avatar className="h-8 w-8">
+                <AvatarImage
+                src={user?.profileImageUrl}
+                 />
+                 <AvatarFallback>
+                    {user?.firstName?.charAt(0)}
+                    {user?.lastName?.charAt(0)}
+                 </AvatarFallback>
+            </Avatar>
+        )
+    }
